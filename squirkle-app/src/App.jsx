85fcfd28bdc69a73a7a2ea0ea445e0f807e7f04e
SagiBeno <benosagi321@gmail.com>
2026-01-29 10:01:37 +0100

    Add default routes

--- squirkle-app/src/App.jsx
+++ squirkle-app/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Game from './components/Game'
+import { Theme } from '@radix-ui/themes';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // TODO - Login
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  var navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loggedIn) navigate('/login');
+  }, []);
 
   return (
     <>
-      <Game />
+      <Theme>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          {
+            !loggedIn &&
+              <>
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage />} />
+              </>
+          }
+        </Routes>
+       
+      </Theme>
+      
     </>
   )
 }
