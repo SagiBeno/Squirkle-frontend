fbf50bac0b4babe91fd57e7925508a72a0693509
Kristoff Red <nitrogen.studios.yt@gmail.com>
2026-02-05 10:25:34 +0100

    Version check and game unzipping

--- squirkle-app/src/App.jsx
+++ squirkle-app/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Game from './components/Game'
+import GameWindow from './components/GameWindow'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Game />
+      <GameWindow />
     </>
   )
 }
