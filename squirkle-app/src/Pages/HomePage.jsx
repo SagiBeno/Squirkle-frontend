88afdf3a408b7fe130e4db9b2289529fe024052e
SagiBeno <benosagi321@gmail.com>
2026-02-12 07:59:42 +0100

    Modify HomePage's layout

--- squirkle-app/src/Pages/HomePage.jsx
+++ squirkle-app/src/Pages/HomePage.jsx
@@ -1,10 +1,31 @@
 import { Box, Flex } from '@radix-ui/themes';
 import GameWindow from '../components/GameWindow';
+import Navbar from '../components/Navbar';
 
 export default function HomePage() {
     return (
-
-        <GameWindow />
+        <Flex
+            style={{
+                width: '100%',
+                height: '100%',
+                flexDirection: 'column',
+            }}
+        >
+            <Navbar />
+            <Flex
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignContent: 'center'
+                }}
+            >
+                <GameWindow />
+            </Flex>
+        </Flex>
+        
 
 
     )
