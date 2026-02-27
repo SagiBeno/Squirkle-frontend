79192cc325dae0f20457797659feb6a64e451dd7
SagiBeno <benosagi321@gmail.com>
2026-02-27 08:42:32 +0100

    Modify HomePage

--- squirkle-app/src/Pages/HomePage.jsx
+++ squirkle-app/src/Pages/HomePage.jsx
@@ -1,15 +1,55 @@
-import { Box, Flex, Text } from '@radix-ui/themes';
+import { Box, Flex, Text, Button } from '@radix-ui/themes';
 import GameWindow from '../components/GameWindow';
 import Navbar from '../components/Navbar';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+    const navigate = useNavigate();
+
     return (
         <Flex className='mainContainer'>
 
             <Box className='navbarSpacer' />
             
             <Flex className='contentContainer'>
-                <Text>Home</Text>
+                
+                <Flex
+                    style={{
+                        margin: "auto",
+                        flexDirection: 'column',
+                        textAlign: 'center',
+                    }}
+                >
+                    <Text size="9" style={{color: 'white', fontWeight: '500'}}>Squirkle</Text>
+                    <Flex
+                        style={{
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                            justifyContent: 'center',
+                            marginTop: '10px'
+                        }}
+                    >
+                        <Button
+                            size="4"
+                            radius='full'
+                            style={{
+                                marginRight: '5px'
+                            }}
+                            onClick={() => navigate("/login")}
+                        >
+                            Sign in
+                        </Button>
+
+                        <Button
+                            size="4"
+                            radius='full'
+                            onClick={() => navigate("/register")}
+                        >
+                            Sign up
+                        </Button>
+                    </Flex>
+                </Flex>
+
             </Flex>
         </Flex>
     )
