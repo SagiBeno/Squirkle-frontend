b7208f0fe58b29f7382a33168aee16808dce4d46
SagiBeno <benosagi321@gmail.com>
2026-04-06 13:57:55 +0200

    Creating a style

--- squirkle-app/src/components/DeleteAlert.jsx
+++ squirkle-app/src/components/DeleteAlert.jsx
@@ -1,12 +1,25 @@
-import { AlertDialog, Button, Flex, DataList, Code } from "@radix-ui/themes"
+import { AlertDialog, Button, Flex, DataList, Code } from "@radix-ui/themes";
+import { FaTrash } from "react-icons/fa";
+import { TbCancel } from "react-icons/tb";
 
 export default function DeleteAlert({ open, setOpen, data, itemID, handleDelete }) {
 
     return (
         <AlertDialog.Root open={open} setOpen={setOpen}>
-            <AlertDialog.Content maxWidth="450px">
+            <AlertDialog.Content
+                maxWidth="450px"
+                style={{
+                    padding: '20px',
+                    borderRadius: '10px',
+                    overflow: "auto",
+                    background: '#21212c',
+                    fontFamily: `"Fredoka", sans-serif`,
+                    color: 'white',
+                    boxShadow: '0px 0px 5px 2px gray'
+                }}
+            >
                 <AlertDialog.Title>Delete item</AlertDialog.Title>
-                <AlertDialog.Description size="2">
+                <AlertDialog.Description size="3">
                     Are you sure you want to delete the following item?
                 </AlertDialog.Description>
 
@@ -16,15 +29,17 @@ export default function DeleteAlert({ open, setOpen, data, itemID, handleDelete 
                         padding: '10px'
                     }}
                 >
-                    <DataList.Root>
+                    <DataList.Root
+                        
+                    >
                         <DataList.Item>
-                            <DataList.Label minWidth="88px">ID</DataList.Label>
+                            <DataList.Label minWidth="88px" style={{color: 'white'}}>ID</DataList.Label>
                             <DataList.Value>
-                                <Code>{itemID}</Code>
+                                <Code color="amber" variant="solid" highContrast >{itemID}</Code>
                             </DataList.Value>
                         </DataList.Item>
                         <DataList.Item>
-                            <DataList.Label minWidth="88px">Name</DataList.Label>
+                            <DataList.Label minWidth="88px" style={{color: 'white'}}>Name</DataList.Label>
                             <DataList.Value>{data.name}</DataList.Value>
                         </DataList.Item>
                     </DataList.Root>
@@ -37,13 +52,13 @@ export default function DeleteAlert({ open, setOpen, data, itemID, handleDelete 
                         }}
                     >
                         <AlertDialog.Cancel onClick={() => setOpen(false)}>
-                            <Button variant="soft" color="gray" style={{ cursor: "pointer" }}>
-                                Cancel
+                            <Button variant="soft" color="amber" style={{ cursor: "pointer", color: 'white' }}>
+                                <TbCancel /> Cancel
                             </Button>
                         </AlertDialog.Cancel>
                         <AlertDialog.Action>
                             <Button variant="solid" color="red" style={{ cursor: "pointer" }} onClick={handleDelete}>
-                                Delete
+                                <FaTrash /> Delete
                             </Button>
                         </AlertDialog.Action>
                     </Flex>
