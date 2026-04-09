8e8f66a5edd8ae3a8ee180599cadc69f7d04b08f
Kristoff Red <nitrogen.studios.yt@gmail.com>
2026-04-09 11:47:04 +0200

    Inventory loads from backend

--- squirkle-app/src/Pages/GamePage.jsx
+++ squirkle-app/src/Pages/GamePage.jsx
@@ -35,7 +35,7 @@ export default function GamePage({ user, signOut, setShowAppLoader }) {
             case AREA_SELECTOR_STATE:
                 return <AreaSelectorDialog/>
             case INVENTORY_STATE:
-                return <InventoryDialog/>
+                return <InventoryDialog user={user}/>
             case AUCTION_HOUSE_STATE:
                 return <AuctionHouseDialog/>
         }
--- squirkle-app/src/components/Dialogs/InventoryDialog.jsx
+++ squirkle-app/src/components/Dialogs/InventoryDialog.jsx
@@ -3,29 +3,22 @@ import React from 'react'
 import ItemSlot from '../ItemSlot'
 import ItemDetailsDialog from './ItemDetailsDialog'
 import { useState } from 'react'
+import { useEffect } from 'react'
 
-export default function InventoryDialog() {
+export default function InventoryDialog({user}) {
 
-    const testData = {
-        "item": {
-            "name": "Test Weapon",
-            "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium earum inventore dicta optio temporibus exercitationem, quos laudantium sed, aspernatur vel error mollitia quas aliquam maiores, soluta fugit. Amet, laboriosam ipsa!",
-            "knockback": 1.1,
-            "imageUrl": "https://http.cat/404",
-            "type": "Weapon",
-            "stats": {
-                "id": "stats",
-                "circleDamage": 10,
-                "squareDamage": 20,
-                "triangleDamage": 30,
-                "critChance": 20,
-                "critDamage": 1.5,
-                "metadata": [ "ABILITY_SMASH", "ABILITY_BURST" ]
-            }
-        }
+    const [ inventory, setInventory ] = useState(null)
+    const [ selectedItem, setSelectedItem ] = useState(null)
+
+    async function GetPlayerInventory()
+    {
+        const json = await (await fetch(`https://squirkle-backend.vercel.app/api/get-inventory/${user.user.uid}`)).json()
+        setInventory(json.items)
     }
 
-    const [ selectedItem, setSelectedItem ] = useState(null)
+    useEffect(() => {
+        GetPlayerInventory()
+    }, [])
 
     return (
         <Dialog.Content width="90vw" maxWidth="920px" height="80vh" style={{ padding: 0, borderRadius: 0, boxShadow: "none", backgroundColor: "transparent", overflow: "auto" }}>
@@ -33,12 +26,9 @@ export default function InventoryDialog() {
 
             <Dialog.Root>
                 <Flex wrap="wrap" justify="start" gap="2">
-                    <ItemSlot itemData={testData.item} onClick={setSelectedItem} />
-                    <ItemSlot itemData={testData.item} onClick={setSelectedItem} />
-                    <ItemSlot itemData={testData.item} onClick={setSelectedItem} />
-                    <ItemSlot itemData={testData.item} onClick={setSelectedItem} />
-                    <ItemSlot itemData={testData.item} onClick={setSelectedItem} />
-                    <ItemSlot itemData={testData.item} onClick={setSelectedItem} />
+                {
+                    inventory?.map((x, i) => <ItemSlot key={x.itemId + i} itemData={x} onClick={setSelectedItem} />)
+                }
                 </Flex>
                 <ItemDetailsDialog itemData={selectedItem}/>
             </Dialog.Root>
