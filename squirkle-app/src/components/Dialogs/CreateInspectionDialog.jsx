b12f9c6740a070ab40c662f941cfa3403d25ea0b
SagiBeno <benosagi321@gmail.com>
2026-04-09 19:16:16 +0200

    Outsourcing the Create Inspection feature to a separate component

---
+++ squirkle-app/src/components/Dialogs/CreateInspectionDialog.jsx
@@ -0,0 +1,21 @@
+import ItemDetailsDialog from "./ItemDetailsDialog";
+import { Dialog, Flex, Text, Heading } from "@radix-ui/themes";
+
+export default function CreateInspectionDialog( { open, setOpen, selectedCreateItem, createListingForm } ) {
+    return (
+        <Dialog.Root open={isCreateInspectOpen} onOpenChange={setIsCreateInspectOpen}>
+            <ItemDetailsDialog
+                itemData={selectedCreateItem}
+                rightPanelExtra={
+                    selectedCreateItem ? (
+                        <Flex direction="column" gap="2" mt="2" style={{ width: '100%' }}>
+                            <Text size="2" color="gray">Selected for listing</Text>
+                            <Text size="2" color="gray">Type: {selectedCreateItem.type}</Text>
+                            <Heading size="4">Set Price: {createListingForm.price || '-'}</Heading>
+                        </Flex>
+                    ) : null
+                }
+            />
+        </Dialog.Root>
+    )
+}
